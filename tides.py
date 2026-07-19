aec340d4f81119f1884ca43945ea052b0bd9b772
chukotkagpt <chukotkagpt@gmail.com>
2026-07-20 00:53:24 +1200

    Update tides.py

--- tides.py
+++ tides.py
@@ -1,11 +1,46 @@
+import os
+import httpx
+
+LAT = 69.7008
+LON = 170.3133
+
+TIDES_API_KEY = os.environ["TIDES_API_KEY"]
+
+
 async def get_tides():
-    """
-    Временная заглушка.
-    Позже здесь будет получение приливов из TidesAtlas.
-    """
+
+    url = (
+        "https://tidesatlas.com/api/v1/tides"
+        f"?lat={LAT}"
+        f"&lon={LON}"
+        "&days=1"
+    )
+
+    headers = {
+        "X-API-Key": TIDES_API_KEY
+    }
+
+    async with httpx.AsyncClient(timeout=20) as client:
+        response = await client.get(url, headers=headers)
+        response.raise_for_status()
+
+    data = response.json()
+
+    extremes = data.get("extremes", [])
+
+    high = None
+    low = None
+
+    for tide in extremes:
+
+        if tide["type"] == "high" and high is None:
+            high = tide["datetime"][11:16]
+
+        elif tide["type"] == "low" and low is None:
+            low = tide["datetime"][11:16]
 
     return {
-        "state": None,
-        "high": None,
-        "low": None,
+        "high": high,
+        "low": low,
+        "state": None
     }
