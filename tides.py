ec9b42dc8c34771348c91c5456aa475cba825f01
chukotkagpt <chukotkagpt@gmail.com>
2026-07-20 00:12:43 +1200

    Create tides.py

---
+++ tides.py
@@ -0,0 +1,11 @@
+async def get_tides():
+    """
+    Временная заглушка.
+    Позже здесь будет получение приливов из TidesAtlas.
+    """
+
+    return {
+        "state": None,
+        "high": None,
+        "low": None,
+    }
